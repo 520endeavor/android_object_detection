8d7e2a20c91c58d44405fbd5451cd670e3638701
endeavor.zhou <2365898474@qq.com>
2021-10-26 01:34:12 +0800

    creat a project including C++

---
+++ app/src/main/java/com/example/object_detection/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.object_detection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("darknet-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native void inityolo(String cfgfile, String weightfile);
+    public native double testyolo(String imgfile);
+    public native boolean detectimg(Bitmap dst, Bitmap src);
+
+}
